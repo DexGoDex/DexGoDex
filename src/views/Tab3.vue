4193d02110b3f48f793995462fae1378944f3fff
AMIR_SYAWAL <syawalamir24@gmail.com>
2021-08-21 21:55:13 +0700

    update

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -1,28 +1,62 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
-      </ion-toolbar>
-    </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title class="ion-text-center">
+            Untuk melakukan reservasi, hubungi kontak dibawah ini</ion-title
+          >
         </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="Tab 3 page" />
+      <div class="ion-text-center">
+        <img src="/assets/images/illustration.png" />
+        <p>
+          Untuk melakukan reservasi,<br />
+          hubungi kontak dibawah ini
+        </p>
+      </div>
+      <div class="ion-text-center">
+        <ion-button>
+          <ion-icon :icon="call" />
+          <ion-label>Reservasi</ion-label>
+        </ion-button>
+      </div>
+      <div class="ion-text-center">
+        <p>
+          Selain yg ada diaplikasi ini, semuanya penipuan
+        </p>
+      </div>
+      <div class="ion-text-center">
+        <p>
+          Sosial media kami :
+        </p>
+        <ion-icon name="logo-instagram"></ion-icon>
+        <ion-icon name="logo-twitter"></ion-icon>
+        <ion-icon name="logo-facebook"></ion-icon>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonTitle, IonContent } from "@ionic/vue";
+import { call, logoInstagram, logoTwitter, logoFacebook } from "ionicons/icons";
 
-export default  {
-  name: 'Tab3',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+export default {
+  name: "Tab3",
+  components: {
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
+  setup() {
+    return {
+      call,
+      logoInstagram,
+      logoTwitter,
+      logoFacebook,
+    };
+  },
+};
 </script>
