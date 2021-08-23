87e34a129da16b75705e0e8365b71c40bd5a3d0d
AMIR_SYAWAL <syawalamir24@gmail.com>
2021-08-23 13:53:23 +0700

    update

--- src/views/page1.vue
+++ src/views/page1.vue
@@ -7,23 +7,45 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/xwkwEGFyc7o"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; 
+      autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <ion-title class="ion-text-left"> Judul</ion-title>
+      <ion-icon :icon="navigate">barat</ion-icon>
+      <ion-title class="ion-text-left"> Deskripsi</ion-title>
+      <div class="ion-text-left">
         <p>
-          Start with Ionic
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://ionicframework.com/docs/components"
-            >UI Components</a
-          >
+          penjelasan mengenai wisata
         </p>
+      </div>
+      <ion-slides pager="true" :options="slideOpts">
+        <ion-slide>
+          <img src="assets/icon/favicon.png" />
+        </ion-slide>
+        <ion-slide>
+          <img src="assets/icon/favicon.png" />
+        </ion-slide>
+      </ion-slides>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26648.164843387858!2d108.19063136167648!3d-6.857798560514725!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f2f0436dc0d17%3A0xbfd45e9dac204617!2sSidamukti%2C%20Kec.%20Majalengka%2C%20Kabupaten%20Majalengka%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1629470348778!5m2!1sid!2sid"
+        width="100%"
+        height="99%"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+      ></iframe>
+      <div class="ion-text-center">
+        <ion-button>
+          <ion-icon :icon="call" />
+          <ion-label>Reservasi</ion-label>
+        </ion-button>
       </div>
     </ion-content>
   </ion-page>
@@ -31,6 +53,8 @@
 
 <script lang="ts">
 import {
+  IonSlide,
+  IonSlides,
   IonContent,
   IonHeader,
   IonPage,
@@ -38,15 +62,28 @@ import {
   IonToolbar,
 } from "@ionic/vue";
 import { defineComponent } from "vue";
+import { navigate, call } from "ionicons/icons";
 
 export default defineComponent({
   name: "Home",
   components: {
+    IonSlide,
+    IonSlides,
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
     IonToolbar,
+  },
+  setup() {
+    const slideOpts = {
+      initialSlide: 1,
+      speed: 400,
+    };
+    return {
+      navigate,
+      call,
+    };
   },
 });
 </script>
