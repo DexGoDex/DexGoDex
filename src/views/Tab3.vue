ad1c3fe4e31206df6fd192238fff38ae3e7f3031
RiskiWR <riskiwahyura@gmail.com>
2021-08-25 21:09:37 +0700

    Update Revervasi

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -1,32 +1,35 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      <div class="ion-text-center">
-        <ion-img src="assets/img/Illustration.png"> </ion-img>
-      </div>
-      <div class="ion-text-center">
-        <p>
-          Untuk melakukan reservasi,<br />
-          hubungi kontak dibawah ini
-        </p>
-      </div>
-      <div class="ion-text-center">
-        <ion-button>
-          <ion-icon :icon="call" />
-          <ion-label>Reservasi</ion-label>
-        </ion-button>
-      </div>
-      <div class="ion-text-center">
-        <p>Selain yg ada diaplikasi ini, <br />semuanya penipuan</p>
-      </div>
-      <div class="ion-text-center">
-        <p>
-          Sosial media kami :
-        </p>
-        <ion-icon :icon="logoInstagram"></ion-icon>
-        <ion-icon :icon="logoTwitter"></ion-icon>
-        <ion-icon :icon="logoFacebook"></ion-icon>
-      </div>
+      <section>
+        <div class="ion-text-center">
+          <img src="assets/img/Illustration.png"> 
+        </div>
+        <div class="ion-text-center">
+          <p>
+            Untuk melakukan reservasi,<br />
+            hubungi kontak dibawah ini
+          </p>
+        </div>
+        <div class="ion-text-center">
+          <ion-button >
+            <ion-icon :icon="call" style="margin-right:10px" />
+            <ion-label><b>Reservasi</b></ion-label>
+          </ion-button>
+        </div>
+        <div class="ion-text-center"  style="margin-bottom: 25px">
+          <small style="color:rgba(58,58,58,40%)">Selain yg ada diaplikasi ini, semuanya penipuan</small>
+        </div>
+        <div class="ion-text-center" id="social-media">
+          <p>
+            Sosial media kami :
+          </p>
+          <ion-icon :icon="logoInstagram"></ion-icon>
+          <ion-icon :icon="logoTwitter"></ion-icon>
+          <ion-icon :icon="logoFacebook"></ion-icon>
+        </div>
+      </section>
+      
     </ion-content>
   </ion-page>
 </template>
@@ -59,7 +62,24 @@ export default {
 };
 </script>
 <style>
+section{
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 ion-img {
   width: 50%;
+}
+#social-media ion-icon{
+  margin: 0 10px;
+  font-size: 24px;
+  color: #088b61;
+}
+ion-button{
+  height: 60px;
+  width: 300px;
+  --border-radius	: 16px;
 }
 </style>
