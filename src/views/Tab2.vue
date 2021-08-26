0e2b9c25067462032be732695ddc88d5bb59103a
AMIR_SYAWAL <syawalamir24@gmail.com>
2021-08-26 19:47:28 +0700

    update

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,33 +1,21 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Map</ion-title>
-      </ion-toolbar>
-    </ion-header>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26648.164843387858!2d108.19063136167648!3d-6.857798560514725!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f2f0436dc0d17%3A0xbfd45e9dac204617!2sSidamukti%2C%20Kec.%20Majalengka%2C%20Kabupaten%20Majalengka%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1629470348778!5m2!1sid!2sid"
-        width="100%"
-        height="100%"
-        style="border:0;"
-        allowfullscreen=""
-        loading="lazy"
-        gestureHandling="greedy"
-      ></iframe>
-  
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d26648.164843387858!2d108.19063136167648!3d-6.857798560514725!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f2f0436dc0d17%3A0xbfd45e9dac204617!2sSidamukti%2C%20Kec.%20Majalengka%2C%20Kabupaten%20Majalengka%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1629470348778!5m2!1sid!2sid"
+      width="100%"
+      height="100%"
+      style="border:0;"
+      allowfullscreen=""
+      loading="lazy"
+      gestureHandling="greedy"
+    ></iframe>
   </ion-page>
 </template>
 
 <script lang="ts">
-
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-} from "@ionic/vue";
+import { IonPage } from "@ionic/vue";
 export default {
   name: "Tab2",
-  components: { IonHeader, IonToolbar, IonTitle, IonPage },
+  components: { IonPage },
 };
 </script>
